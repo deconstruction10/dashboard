d24d82199fda8a363fb943ad2b360c2c38885478
deconstruction10 <andrew.bartchuk@gmail.com>
2021-10-08 01:43:52 +0300

    added lazy loading routing

--- src/app/feature/navbar-routing.module.ts
+++ src/app/feature/navbar-routing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import {Routes} from "@angular/router";
 import {RouterModule} from "@angular/router";
 import {AuthorizationModule} from "../auth/authorization.module";
+import {LoginComponent} from "../modules/general/login/login/login.component";
+import {RegisterComponent} from "../modules/general/register/register/register.component";
 
 const routes: Routes = [
   {path: 'auth', children: [
-      {path: 'login', loadChildren: () => import('../auth/authorization.module').then(m => AuthorizationModule)},
-      {path: 'register', loadChildren: () => import('../auth/authorization.module').then(m => AuthorizationModule)},
+      {path: 'login', component: LoginComponent, loadChildren: () => import('../auth/authorization.module').then(m => AuthorizationModule)},
+      {path: 'register', component: RegisterComponent, loadChildren: () => import('../auth/authorization.module').then(m => AuthorizationModule)},
     ]
   }
 ];
